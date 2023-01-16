a7c8ad3e3763068f6bac3b029fd8c2669afb9acc
Olena Grynevych <olena.grynevych@scandiweb.com>
2023-01-16 09:34:36 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import { HomePage } from './routes/home-page.component';
+
+function App() {
+  return <HomePage />
+}
+
+export default App;
---
+++ src/routes/home-page.component.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div className='main-content'>
+            Hello world something new
+        </div>
+    );
+}
+
+export { HomePage };
